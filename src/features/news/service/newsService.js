a14326ca9606a5b75f3bb12bbc6951486db795b5
Gentahall <gentahalilintar36@gmail.com>
2025-09-23 15:39:59 +0700

    update: hide source api

--- src/features/news/service/newsService.js
+++ src/features/news/service/newsService.js
@@ -4,19 +4,19 @@ const BASE_URL = "/blogs";
 
 export const newsService = {
   getAllNews: async () => {
-    try {
-      const response = await api.get(BASE_URL);
-      return response.data.data.data; 
-    } catch (error) {
-      throw error;
-    }
+    // try {
+    //   const response = await api.get(BASE_URL);
+    //   return response.data.data.data; 
+    // } catch (error) {
+    //   throw error;
+    // }
   },
   getNewsById: async (id) => {
-    try {
-      const response = await api.get(`${BASE_URL}/${id}`);
-      return response.data.data;
-    } catch (error) {
-      throw error;
-    }
+    // try {
+    //   const response = await api.get(`${BASE_URL}/${id}`);
+    //   return response.data.data;
+    // } catch (error) {
+    //   throw error;
+    // }
   }
 }; 
